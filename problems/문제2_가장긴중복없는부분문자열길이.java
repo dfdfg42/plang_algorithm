31c815c8c3c57c4d8bcc06639d54454afb2446e4
고경우 <dfdfg11@gmail.com>
2026-02-22 20:19:41 +0900

    feat: 문제 2 구현

--- problems/문제2_가장긴중복없는부분문자열길이.java
+++ problems/문제2_가장긴중복없는부분문자열길이.java
@@ -1,12 +1,33 @@
 public class 문제2_가장긴중복없는부분문자열길이 {
     public static void main(String[] args) {
-        // TODO: 문제 2 풀이 작성
+        StringBuilder sb = new StringBuilder();
+        sb.append(solution("abcabcbb") );
+        sb.append('\n');
+        sb.append(solution("bbbbb") );
+        sb.append('\n');
+        sb.append(solution("pwwkew"));
+        sb.append('\n');
+        sb.append(solution(""));
+        sb.append('\n');
+
+        System.out.println(sb);
     }
     
-    public static String solution(String s){
+    public static int solution(String s){
+        
+        int[] cache = new int[128];
+        int len =0 ;
+        int l =0;
 
+        for (int r = 0; r < s.length(); r++){
+            cache[s.charAt(r) ]++;
+            while(cache[s.charAt(r)] >1){
+                cache[s.charAt(l++)]--;
+            }
+            len = Math.max(len,r-l +1 );
+        }
 
-        return s;
+        return len;
 
     }
 }
